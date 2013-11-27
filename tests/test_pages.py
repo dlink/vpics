3d38cbb3bb3a0cf8fe3507dd54175dd5c741eac6
David Link <dlink@gmail.com>
2013-11-27 22:48:14 +0000

    New test for getting page via name

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -5,19 +5,24 @@ import unittest
 from pages import Pages, Page
 
 PAGE_ID = 1
+PAGE_NAME = 'Sculpture'
 
 class TestPages(unittest.TestCase):
 
     def setUp(self):
         self.pages = Pages()
-        self.page  = Page(PAGE_ID)
 
     def test_pages_list_all(self):
         num = len(self.pages.get())
         self.assertTrue(num)
 
     def test_page_get(self):
-        self.assertTrue(PAGE_ID, self.page.page_id)
+        page = Page(PAGE_ID)
+        self.assertTrue(PAGE_ID, page.page_id)
 
+    def test_page_get_name(self):
+        page = self.pages.get(filter="name = '%s'" % PAGE_NAME)[0]
+        self.assertTrue(PAGE_NAME, page.name)
+                           
 if __name__ == '__main__':
     unittest.main()
