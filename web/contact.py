4f259c9c5ba99b17ac8867d0bd69bc8bc9ea59f8
David Link <dlink@gmail.com>
2013-12-01 06:30:48 +0000

    New Contact Info page

---
+++ web/contact.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from vlib import conf
+
+from vweb.htmlpage import HtmlPage
+from vweb.htmltable import HtmlTable
+from vweb.html import *
+
+from nav import Nav
+
+class Contact(HtmlPage):
+
+    def __init__(self):
+        HtmlPage.__init__(self, 'Pics Page')
+        self.style_sheets.append('css/vpics.css')
+        #self.style_sheets.append('css/contact.css')
+
+        # navigation
+        self.nav = Nav()
+        self.style_sheets.append(self.nav.style_sheets())
+
+    def getHtmlContent(self):
+        return \
+            self.header() +\
+            self.navAndDisplayArea()
+                       
+    def navAndDisplayArea(self):
+        table = HtmlTable()
+        table.addRow([self.nav.nav(name='Contact Info'),
+                      self.displayArea()])
+        table.setRowVAlign(1,'top')
+        return center(table.getTable())
+
+    def header(self):
+        text = 'David Link'
+        return div(h2(text), id='header')
+
+    def displayArea(self):
+        return div(text(), id='displayArea')
+
+def text():
+    return '''
+David Link<br>
+Putnam Valley, NY<br>
+<br>
+914-629-5723<br>
+<br>
+dvlink@gmail.com<br>
+
+'''
+
+Contact().go()
+
