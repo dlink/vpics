677da9f04c44b8b821efd08bbf83f8413d7c3551
David Link <dlink@gmail.com>
2014-10-27 00:38:35 +0000

    index: removed setting of sys.path

--- web/index.py
+++ web/index.py
@@ -4,8 +4,8 @@ import os
 import sys
 
 # add vpics lib to PYTHONPATH:
-libdir = os.path.dirname(os.path.realpath(__file__)).replace('/web', '/lib')
-sys.path.append(libdir)
+#libdir = os.path.dirname(os.path.realpath(__file__)).replace('/web', '/lib')
+#sys.path.append(libdir)
 
 from collection import Collection
 Collection().go()
